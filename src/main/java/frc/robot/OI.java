6064cf1dcc5fd79a4de8d963e077ef620cb1f1fd
symonssoftware <56047819+symonssoftware@users.noreply.github.com>
2020-01-12 17:56:16 -0500

    Changed xboxIF member variable naming to match what WPI uses.

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -36,108 +36,108 @@ public class OI {
     // until it is finished as determined by it's isFinished method.
     // button.whenReleased(new ExampleCommand());
 
-    private static XboxController xboxIF;
+    private static XboxController m_xboxController;
 
     public OI() {
 
-        xboxIF = new XboxController(0);
+        m_xboxController = new XboxController(0);
     }
 
     public static boolean getXboxAButton() {
-        return xboxIF.getAButton();
+        return m_xboxController.getAButton();
     }
 
     public static boolean getXboxBButton() {
-        return xboxIF.getBButton();
+        return m_xboxController.getBButton();
     }
 
     public static boolean getXboxXButton() {
-        return xboxIF.getXButton();
+        return m_xboxController.getXButton();
     }
 
     public static boolean getXboxYButton() {
-        return xboxIF.getYButton();
+        return m_xboxController.getYButton();
     }
 
     public static boolean getXboxStartButton() {
-        return xboxIF.getStartButton();
+        return m_xboxController.getStartButton();
     }
 
     public static boolean getXboxBackButton() {
-        return xboxIF.getBackButton();
+        return m_xboxController.getBackButton();
     }
 
     public static boolean getXboxRightBumper() {
-        return xboxIF.getBumper(Hand.kRight);
+        return m_xboxController.getBumper(Hand.kRight);
     }
 
     public static boolean getXboxLeftBumper() {
-        return xboxIF.getBumper(Hand.kLeft);
+        return m_xboxController.getBumper(Hand.kLeft);
     }
 
     public static boolean getXboxLeftJoystickPress() {
-        return xboxIF.getStickButton(Hand.kLeft);
+        return m_xboxController.getStickButton(Hand.kLeft);
     }
 
     public boolean getXboxRightJoystickPress() {
-        return xboxIF.getStickButton(Hand.kRight);
+        return m_xboxController.getStickButton(Hand.kRight);
     }
 
     public static double getXboxRightTrigger() {
-        return xboxIF.getTriggerAxis(Hand.kRight);
+        return m_xboxController.getTriggerAxis(Hand.kRight);
     }
 
     public static double getXboxLeftTrigger() {
-        return xboxIF.getTriggerAxis(Hand.kLeft);
+        return m_xboxController.getTriggerAxis(Hand.kLeft);
     }
 
     public static double getXboxRightJoystickX() {
-        return xboxIF.getX(Hand.kRight);
+        return m_xboxController.getX(Hand.kRight);
     }
 
     public static double getXboxRightJoystickY() {
-        return xboxIF.getY(Hand.kRight);
+        return m_xboxController.getY(Hand.kRight);
     }
 
     public static double getXboxLeftJoystickX() {
-        return xboxIF.getX(Hand.kLeft);
+        return m_xboxController.getX(Hand.kLeft);
     }
 
     public static double getXboxLeftJoystickY() {
-        return xboxIF.getY(Hand.kLeft);
+        return m_xboxController.getY(Hand.kLeft);
     }
 
     public static boolean getXboxDpadUp() {
-        int pov = xboxIF.getPOV(0);
+        int pov = m_xboxController.getPOV(0);
         return (((pov >= 0) && (pov <= 45)) || ((pov >= 315) && (pov <= 360)));
     }
 
     public static boolean getXboxDpadRight() {
-        int pov = xboxIF.getPOV(0);
+        int pov = m_xboxController.getPOV(0);
         return ((pov >= 45) && (pov <= 135));
     }
 
     public static boolean getXboxDpadDown() {
-        int pov = xboxIF.getPOV(0);
+        int pov = m_xboxController.getPOV(0);
         return ((pov >= 135) && (pov <= 225));
     }
 
     public static boolean getXboxDpadLeft() {
-        int pov = xboxIF.getPOV(0);
+        int pov = m_xboxController.getPOV(0);
         return ((pov >= 225) && (pov <= 315));
     }
 
     public static void setXboxRumbleSpeed(double rumbleSpeed) {
-        xboxIF.setRumble(RumbleType.kLeftRumble, rumbleSpeed);
-        xboxIF.setRumble(RumbleType.kRightRumble, rumbleSpeed);
+        m_xboxController.setRumble(RumbleType.kLeftRumble, rumbleSpeed);
+        m_xboxController.setRumble(RumbleType.kRightRumble, rumbleSpeed);
     }
 
     public static void setXboxLeftRumbleSpeed(double rumbleSpeed) {
-        xboxIF.setRumble(RumbleType.kLeftRumble, rumbleSpeed);
+        m_xboxController.setRumble(RumbleType.kLeftRumble, rumbleSpeed);
     }
 
     public static void setXboxRightRumbleSpeed(double rumbleSpeed) {
-        xboxIF.setRumble(RumbleType.kRightRumble, rumbleSpeed);
+        m_xboxController.setRumble(RumbleType.kRightRumble, rumbleSpeed);
     }
 
     public static void setXboxRumbleStop() {
