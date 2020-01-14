4bdee502eed54467ea0cc3faa417ba9d69b328ba
CougarTech <cougartechsoftware@gmail.com>
2020-01-13 19:56:37 -0500

    Added subsystem stub files

---
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.Constants;
+
+public class ClimberSubsystem extends SubsystemBase {
+
+    public ClimberSubsystem() {
+        
+        // You need to register the subsystem to get it's periodic
+        // method to be called by the Scheduler
+        CommandScheduler.getInstance().registerSubsystem(this);
+
+    }
+
+    @Override
+    public void periodic() {
+        // Put code here to be run every loop
+        
+    }
+
+}
